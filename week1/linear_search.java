9666a91a81a06eb91a9afada80cdb3d5b88ac3ce
Suyash Dubey <dubeykaran786@gmail.com>
2021-09-12 11:40:17 +0530

    Add files via upload

---
+++ week1/linear_search.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class linear_search
+{
+  public static void main(String om[])
+  {
+    int n,size,key;
+    Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+    for(int k=0;k<n;k++)
+    {
+      size=sc.nextInt();
+      int []a=new int[size];
+      for(int j=0;j<size;j++)
+      {
+        a[j]=sc.nextInt();
+      }
+      key=sc.nextInt();
+      int count=0;
+      boolean flag=false;
+      for(int i=0;i<size;i++)
+      {
+        if(a[i]==key)
+        {
+          count++;
+          flag=true;
+          break;
+        }
+        else count++;
+      }
+      if(flag==true)
+      {
+        System.out.println("Present"+count);
+      }
+      else{
+        System.out.println("Not Present"+count);
+      }
+    }
+
+  }
+}
