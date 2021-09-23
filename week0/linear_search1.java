474cfe6c9637817c97c8d8cbd56a53e7ac50edda
Suyash Dubey <dubeykaran786@gmail.com>
2021-09-23 11:00:02 +0530

    Update linear_search1.java

--- week0/linear_search1.java
+++ week0/linear_search1.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+//SUYASH DUBEY    SECTION C    2014907
 class linear_search1
 {
 public static void main(String om[])
@@ -6,6 +7,7 @@ public static void main(String om[])
 System.out.println("Enter size of array: ");
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
+  //SUYASH DUBEY    SECTION C    2014907
 int a[]=new int[n];
 System.out.println("Enter elements: ");
 for(int i=0;i<n;i++)
@@ -15,6 +17,7 @@ for(int i=0;i<n;i++)
 System.out.println("Enter an element to search: ");
 int e=sc.nextInt();
 boolean flag=false;
+  //SUYASH DUBEY    SECTION C    2014907
 int count=0;
 for(int i=0;i<n;i++)
 {
@@ -23,16 +26,19 @@ for(int i=0;i<n;i++)
     flag=true;
     break;
   }
+  //SUYASH DUBEY    SECTION C    2014907
   else
   { flag=false;
     count++;
   }
 }
+  //SUYASH DUBEY    SECTION C    2014907
 if(flag)
 System.out.println("ELEMENT FOUND");
 else
 System.out.println("ELEMENT NOT FOUND");
 System.out.println("Number of comparisons: "+count);
+  //SUYASH DUBEY    SECTION C    2014907
 
 }
 }
